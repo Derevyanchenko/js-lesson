80d1dd803aaf2183aa052616e1d0e519e7da7c4e
Unknown <danilderevyanchenko@gmail.com>
2018-09-29 23:50:32 +0300

    написал табы и попапы

---
+++ yoga/js/script.js
@@ -0,0 +1,64 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+	let tab 		= document.getElementsByClassName("info-header-tab");
+	let tab_content = document.getElementsByClassName("info-tabcontent");
+	let tab_first 	= document.getElementsByClassName("info-header")[0];
+
+
+	function hideTabContent(a) {
+
+		for (let i=a; i<tab_content.length; i++) {
+			tab_content[i].classList.remove("show");
+			tab_content[i].classList.add("hide");
+		}
+
+	}
+
+	hideTabContent(1)
+
+	function showTabContent(b) {
+		if(tab_content[b].classList.contains("hide")) {
+			hideTabContent(0);
+			tab_content[b].classList.remove("hide");
+			tab_content[b].classList.add("show");
+		}
+	}
+
+	tab_first.addEventListener("click", function(event) {
+		let target = event.target;
+
+		if(target.className = "info-header-tab") {
+			for(let i = 0; i < tab.length; i++) {
+				if(target == tab[i]) {
+					showTabContent(i);
+					break;
+				}
+			}
+		};
+	});
+
+	// popup
+
+	let more    = document.querySelector(".more"),
+		close   = document.querySelector(".popup-close"),
+		body    = document.querySelector("body"),
+		more_btn = document.querySelector(".description-btn"),
+		overlay = document.querySelector(".overlay");
+
+	more.addEventListener("click", function() {
+		overlay.style.display = "block";
+		body.style.overflow = "hidden";
+	});
+
+	more_btn.addEventListener("click", function() {
+		overlay.style.display = "block";
+		body.style.overflow = "hidden";
+	});
+
+	close.addEventListener("click", function() {
+		overlay.style.display = "none";
+		body.style.overflow = "";
+	});
+
+});
+
